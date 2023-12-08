709643c0d7d5a1fdc724ed57547a14e5e6a0a693
S0meguy <s0meguy1.sh@gmail.com>
2023-12-08 10:30:18 -0500

    Update utils.js
    Added Slack integration

--- utils.js
+++ utils.js
@@ -3,35 +3,41 @@ var CONFIG = require(process.cwd() + '/config.json');
 //Init logger
 var log4js = require('log4js');
 log4js.configure({
-  appenders: {
-    out: { type: 'console' }, 
-    info: { type: 'file', filename: './log/' + CONFIG.logFileName + '.log' },
-    debug: { type: 'file', filename: './log/' + CONFIG.logFileName + '_debug.log' }
-  },
-  categories: {
-    default: { appenders: ['out','info'], level: 'info' },
-    debug: { appenders: ['out','debug'], level: 'info' }
-  }
-  });    
-module.exports.logger = log4js.getLogger('default'); 
-module.exports.loggerDebug = log4js.getLogger('debug'); 
-
-//Init nodemailer
-const nodemailer = require('nodemailer');
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: CONFIG.emailSender,
-      pass: CONFIG.passEmailSender
+    appenders: {
+        out: { type: 'console' },
+        info: { type: 'file', filename: './log/' + CONFIG.logFileName + '.log' },
+        debug: { type: 'file', filename: './log/' + CONFIG.logFileName + '_debug.log' }
+    },
+    categories: {
+        default: { appenders: ['out', 'info'], level: 'info' },
+        debug: { appenders: ['out', 'debug'], level: 'info' }
     }
-  });  
+});
+module.exports.logger = log4js.getLogger('default');
+module.exports.loggerDebug = log4js.getLogger('debug');
+
+// Slack Web API setup
+const { WebClient } = require('@slack/web-api');
+const slackClient = new WebClient(CONFIG.slackToken);
+
+// Function to send a message to Slack
+async function sendSlackMessage(channel, message) {
+    try {
+        await slackClient.chat.postMessage({
+            channel: channel,
+            text: message
+        });
+    } catch (error) {
+        loggerDebug.info('Error sending message to Slack:', error);
+    }
+}
 
 module.exports.sleep = function sleep(s) {
-    return new Promise(resolve => setTimeout(resolve, s*1000), rejected => {});
+    return new Promise(resolve => setTimeout(resolve, s * 1000), rejected => {});
 }
 
 function getDate() {
-    return Date.now()/1000;
+    return Date.now() / 1000;
 }
 
 module.exports.getDate = getDate;
@@ -39,12 +45,12 @@ module.exports.getDate = getDate;
 function readLogFile() {
     var fs = require('fs');
 
-    return new Promise(function(resolve, reject) {
-        fs.readFile('./log/' + CONFIG.logFileName + '.log', 'utf8', function(err, data) {
-            if(err) { 
-                reject(err);  
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./log/' + CONFIG.logFileName + '.log', 'utf8', function (err, data) {
+            if (err) {
+                reject(err);
             }
-            else {              
+            else {
                 resolve(data);
             }
         });
@@ -52,72 +58,60 @@ function readLogFile() {
 }
 
 async function logToEmail() {
-    let dataLog = await readLogFile()  
-    .then(data => {
-        return data.toString().split("\n");
-    })
-    .catch(err => {
-        throw(err);
-    });  
+    let dataLog = await readLogFile()
+        .then(data => {
+            return data.toString().split("\n");
+        })
+        .catch(err => {
+            throw (err);
+        });
 
-    dataLog = dataLog.slice(Math.max(dataLog.length - CONFIG.nbLineLogEmail, 0));  
+    dataLog = dataLog.slice(Math.max(dataLog.length - CONFIG.nbLineLogEmail, 0));
     dataLog = dataLog.reverse();
-    dataLog = dataLog.toString().replace(/,/g, "\n");  
+    dataLog = dataLog.toString().replace(/,/g, "\n");
 
-    return dataLog;  
+    return dataLog;
 }
-    
-module.exports.sendEmail = async function sendEmail(message, api) {
+
+module.exports.sendNotification = async function sendNotification(message, api) {
     let bodyMessage = await logToEmail();
 
-    bodyMessage = bodyMessage + "\n\n" + await getHistoryStat(api);
+    bodyMessage += "\n\n" + await getHistoryStat(api);
 
-    var mailOptions = {
-        from: CONFIG.emailSender,
-        to: CONFIG.emailReceiver,
-        subject: message,
-        text: bodyMessage
-        };
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            loggerDebug.info(error);
-        } else {
-            loggerDebug.info(message + ' Email sent: ' + info.response);
-        }
-        });
+    await sendSlackMessage(CONFIG.slackChannel, message + "\n");
+// + bodyMessage);
 }
-  
-module.exports.saveGraphData = function saveGraphData(monitoredDevice) {  
+
+module.exports.saveGraphData = function saveGraphData(monitoredDevice) {
     let fs = require('fs');
-        
-    let start = monitoredDevice.lastStartedTime;    
+
+    let start = monitoredDevice.lastStartedTime;
     let stop = getDate();
-    let running = stop-start;
-    let startDate = new Date(start*1000);
-    let stopDate = new Date(stop*1000);
+    let running = stop - start;
+    let startDate = new Date(start * 1000);
+    let stopDate = new Date(stop * 1000);
 
     fs.appendFile('./ui/src/assets/' + 'device_graph.csv', startDate.toLocaleDateString() + " " + startDate.toLocaleTimeString() + " , " +
-    stopDate.toLocaleDateString() + " " + stopDate.toLocaleTimeString() + " , " +
-    running + "\n", function (err) {
-        if (err) throw err;
-    });
+        stopDate.toLocaleDateString() + " " + stopDate.toLocaleTimeString() + " , " +
+        running + "\n", function (err) {
+            if (err) throw err;
+        });
 }
-
 
 async function getHistoryStat(api) {
 
     let history = "Global energy consumption for this month and this year\n\n";
 
-    dayStat = await api.getDayStats();  
-    for(data of dayStat.day_list ) {
-        history += JSON.stringify(data) + "\n"        
+    let dayStat = await api.getDayStats();
+    for (let data of dayStat.day_list) {
+        history += JSON.stringify(data) + "\n"
     }
 
     history += "\n";
 
-    monthStat = await api.getMonthStats();  
-    for(data of monthStat.month_list) {
-        history += JSON.stringify(data) + "\n"  
+    let monthStat = await api.getMonthStats();
+    for (let data of monthStat.month_list) {
+        history += JSON.stringify(data) + "\n"
     }
 
     return history;
@@ -125,13 +119,13 @@ async function getHistoryStat(api) {
 
 module.exports.writeHistoryStatToCSV = async function writeHistoryStatToCSV(api) {
     let fs = require('fs');
-        
+
     fs.writeFile('./log/' + CONFIG.logFileName + '_energy_graph.csv', "Global energy consumption\n", function (err) {
         if (err) throw err;
     });
 
-    dayStat = await api.getDayStats();  
-    for(data of dayStat.day_list ) {
+    let dayStat = await api.getDayStats();
+    for (let data of dayStat.day_list) {
         fs.appendFile('./log/' + CONFIG.logFileName + '_energy_graph.csv', JSON.stringify(data) + "\n", function (err) {
             if (err) throw err;
         });
@@ -141,10 +135,10 @@ module.exports.writeHistoryStatToCSV = async function writeHistoryStatToCSV(api)
         if (err) throw err;
     });
 
-    monthStat = await api.getMonthStats();  
-    for(data of monthStat.month_list) {
+    let monthStat = await api.getMonthStats();
+    for (let data of monthStat.month_list) {
         fs.appendFile('./log/' + CONFIG.logFileName + '_energy_graph.csv', JSON.stringify(data) + "\n", function (err) {
             if (err) throw err;
-        });        
+        });
     }
 }
